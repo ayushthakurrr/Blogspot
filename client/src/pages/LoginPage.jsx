0bd61ca9b4b9f11a1d5c57fd08c2efe35774b3bd
Ayush <121556071+ayushthakurrr@users.noreply.github.com>
2024-10-24 19:40:24 +0530

    Update LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -12,7 +12,7 @@ export default function LoginPage() {
   const failed = () => toast.error("Login failed");
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/login', {
+    const response = await fetch('https://blogspot-backend-45r6.onrender.com/login', {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
