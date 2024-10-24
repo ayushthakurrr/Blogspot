6a51efb7854648109dd902290d7d8ce6f587fae2
Ayush <121556071+ayushthakurrr@users.noreply.github.com>
2024-10-24 19:39:37 +0530

    Update EditPost.jsx

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -11,7 +11,7 @@ export default function EditPost() {
   const [redirect,setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:4000/post/'+id)
+    fetch('https://blogspot-backend-45r6.onrender.com/post/'+id)
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
