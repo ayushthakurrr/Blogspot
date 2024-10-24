56950e5b7e85beef4962cd7cf2acce2a3e08926c
Ayush <121556071+ayushthakurrr@users.noreply.github.com>
2024-10-24 20:36:07 +0530

    Update PostPage.jsx

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -9,7 +9,7 @@ export default function PostPage() {
   const {userInfo} = useContext(UserContext);
   const {id} = useParams();
   useEffect(() => {
-    fetch(`https://blogspot-backend-45r6.onrender.com/post/${id}`)
+    fetch(`https://blogspot-backend-3de9.onrender.com/post/${id}`)
       .then(response => {
         response.json().then(postInfo => {
           setPostInfo(postInfo);
