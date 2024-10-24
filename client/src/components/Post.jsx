9abdeee33e89956fd4ff2b2c51edf0923962c081
Ayush <121556071+ayushthakurrr@users.noreply.github.com>
2024-10-24 20:38:06 +0530

    Update Post.jsx

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -7,7 +7,7 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
     <div className="post">
       <div className="image">
         <Link to={`/post/${_id}`}>
-          <img src={'https://blogspot-backend-45r6.onrender.com/'+cover} alt=""/>
+          <img src={'https://blogspot-backend-3de9.onrender.com/'+cover} alt=""/>
         </Link>
       </div>
       <div className="texts">
